336fa5e09700be80cc61b193351a04b3b99e8e71
admin <admin@noreply.com>
2022-05-03 14:58:01 +0800

    upl word lists

--- js/init.js
+++ js/init.js
@@ -1,55 +1,45 @@
-function adjustBody()
-{
-	var mt = $(document.body).height() - ($(".body").height() + 20);	
+function adjustBody() {
+	let mt = $(document.body).height() - ($(".body").height() + 20);
 	$(".body").css("marginTop", 0.5 * mt);
 }
- 
-window.onload = function()
-{
+
+window.onload = function () {
 	adjustBody();
 }
 
-window.onresize = function()
-{
+window.onresize = function () {
 	adjustBody();
 }
 
-function porp(x, y)
-{ 
-	var a = x.Tables_in_wordlist.replace(/[^0-9]/ig, '');
-	var b = y.Tables_in_wordlist.replace(/[^0-9]/ig, '');
-	return parseInt(a) > parseInt(b) ? 1 : -1; 
-}
+// function porp(x, y) {
+// 	let a = x.Tables_in_wordlist.replace(/[^0-9]/ig, '');
+// 	let b = y.Tables_in_wordlist.replace(/[^0-9]/ig, '');
+// 	return parseInt(a) > parseInt(b) ? 1 : -1;
+// }
 
-function loadUnits(domain)
-{
-	var count = 0;
-	var jurl = domain + "/mybackend/wordlist/index.php";
-	
+function loadUnits(domain) {
+	let count = 0;
+	let jurl = domain + "wordlist.json";
+
 	$.ajaxSettings.async = false;
-	$.getJSON(jurl, function (data)
-	{		
-		var uselect = $(".ui-select");
-		data.sort(porp);
-		$.each(data, function(i, item)
-		{
-			if(count == 0)
-			{
-				uselect.append('<option selected value="0">' + item.Tables_in_wordlist + '</option>');
+	$.getJSON(jurl, function (data) {
+		let uselect = $(".ui-select");
+		// data.sort(porp);
+		$.each(data, function (i, item) {
+			if (count == 0) {
+				uselect.append('<option selected value="0">unit' + item.Tables_in_wordlist + '</option>');
 			}
-			else
-			{
-				uselect.append('<option value="' + count.toString() + '">' + item.Tables_in_wordlist + '</option>');
+			else {
+				uselect.append('<option value="' + count.toString() + '">unit' + item.Tables_in_wordlist + '</option>');
 			}
-			
+
 			count++;
 		})
-		
+
 	});
-	
+
 }
 
-$(function()
-{
-	loadUnits("https://georgechou21.asuscomm.com:4443");
+$(function () {
+	loadUnits("./units/");
 });
--- js/list.js
+++ js/list.js
@@ -1,312 +1,316 @@
- 
-function wordlist(ord, list){
-	
+
+function wordlist(ord, list) {
+
 	tl = 0;
 	fl = 20;
 	QA = new Array();
-	
-	var k = 0;
-	var domain = "https://georgechou21.asuscomm.com:4443";
-	var jurl = domain + "/mybackend/wordlist/index.php?list=" + list;
-	
-	$.ajaxSettings.async = false; 
-	$.getJSON(jurl, function (data){
+
+	let k = 0;
+	let domain = "./units/";
+	let jurl = domain + (ord ? "order/" : "disorder/") + list + ".json";
+
+	$.ajaxSettings.async = false;
+	$.getJSON(jurl, function (data) {
 		tl = data.length;
-		var m = mess(k, fragtop(k, tl, fl));
-		$.each(data,function(i, item){ 
-		
-			QA[m[k % fl]] = [item.Question, item.Answer, item.Options];
+		let m = mess(k, fragtop(k, tl, fl));
+		$.each(data, function (i, item) {
+
+			QA[m[k % fl]] = [item.Question, item.Answer, item.Options];//randAns(item.Options, item.Answer)];
 			k++;
-			
+
 			if (k >= top) { return; }
 			else if (k % fl == 0) { m = mess(k, fragtop(k, tl, fl)); }
-			
+
 		})
 	});
-	
-	if (!ord) { QA = disorganize(QA); }
+
+	// if (!ord) { QA = disorganize(QA); }
 }
 
-function fragtop(k, tl, fl){
-	var t = fl * (1 + Math.floor(k / fl));
+function fragtop(k, tl, fl) {
+	let t = fl * (1 + Math.floor(k / fl));
 	return fl - ((t > tl) ? (t - tl) : 0);
 }
- 
-function randAns(opt, ans){
-	return (opt != "0" && Math.random() >= 0.5) ? opt : ans;
-} 
 
-function insElement(id, obj){
-	var e = document.getElementById(id);
+function randAns(opt, ans) {
+	if (opt != "") {
+		return Math.random() >= 0.5 ? opt : ans;
+	}
+	else {
+		return ans;
+	}
+}
+
+function insElement(id, obj) {
+	let e = document.getElementById(id);
 	e.appendChild(obj);
 }
 
-function updElement(id, ans, opt){
-	var kn = 0;
-	var e = document.getElementById(id).childNodes[0];
+function updElement(id, ans, opt) {
+	let kn = 0;
+	let e = document.getElementById(id).childNodes[0];
 	if (e != null) {
-		var type = e.tagName;
-		var inner = e.innerHTML; 
-		var p = e.parentNode;
+		let type = e.tagName;
+		let inner = e.innerHTML;
+		let p = e.parentNode;
 		p.removeChild(e);
-		
-		var en = document.createElement(type);
+
+		let en = document.createElement(type);
 		en.style.color = (inner == ans || inner == opt) ? "green" : "red";
 		en.innerHTML = inner;
 		p.appendChild(en);
-		
+
 		if (en.style.color == "green") { kn = 1; }
 	}
 	return kn;
 }
 
-function clearChilds(id){
-	var e = document.getElementById(id);
-	var childs = e.childNodes;
-	for (var i = 0 ; i < childs.length ; i++){
+function clearChilds(id) {
+	let e = document.getElementById(id);
+	let childs = e.childNodes;
+	for (let i = 0; i < childs.length; i++) {
 		e.removeChild(childs[i]);
 	}
 }
 
-function updChild(id, ans, opt){
+function updChild(id, ans, opt) {
 	clearChilds(id);
-	
-	var e = document.getElementById(id);
-	var c = document.createElement("p");
-	if (opt != "0") { ans += ("/ " + opt); }
+
+	let e = document.getElementById(id);
+	let c = document.createElement("p");
+	if (opt != "") { ans += ("/ " + opt); }
 	c.innerHTML = ans;
 	e.appendChild(c);
 }
 
-function disorganize(arr){
-	var _floor = Math.floor, _random = Math.random, 
-        len = arr.length, i, j, arri, 
-        n = _floor(len / 2) + 1; 
-    while(n--){ 
-        i = _floor(_random() * len); 
-        j = _floor(_random() * len); 
-        if(i !== j){ 
-            arri = arr[i]; 
-            arr[i] = arr[j]; 
-            arr[j] = arri;
-        } 
-    } 
-    return arr;
+function disorganize(arr) {
+	let _floor = Math.floor, _random = Math.random,
+		len = arr.length, i, j, arri,
+		n = _floor(len / 2) + 1;
+	while (n--) {
+		i = _floor(_random() * len);
+		j = _floor(_random() * len);
+		if (i !== j) {
+			arri = arr[i];
+			arr[i] = arr[j];
+			arr[j] = arri;
+		}
+	}
+	return arr;
 }
 
-function mess(s, n){
-	var arr = new Array(n);
-	for (var i = 0 ; i < n ; i++){
+function mess(s, n) {
+	let arr = new Array(n);
+	for (let i = 0; i < n; i++) {
 		arr[i] = s + i;
 	}
-    return disorganize(arr);
-} 
+	return disorganize(arr);
+}
 
 /*------------------------------------------------------------*/
 
-function vocNum(){
-	var obj = document.getElementById("list");
-	var index = obj.selectedIndex;
+function vocNum() {
+	let obj = document.getElementById("list");
+	let index = obj.selectedIndex;
 	return parseInt(obj.options[index].value, 10);
-} 
+}
 
-function chapter(){
+function chapter() {
 	ClearTable();
 	InitTable();
 	loadQA();
-	InitDrag(); 
+	InitDrag();
 	BtnDisable();
 	RecBtn();
 }
 
-function pageUp(){
-	var n = vocNum() - 1;
+function pageUp() {
+	let n = vocNum() - 1;
 	if (n >= 1) {
 		$("#list").val(n);
 		chapter();
 	}
 }
 
-function pageDown(){
-	var n = vocNum() + 1;
-	var top = Math.ceil(tl / fl);
+function pageDown() {
+	let n = vocNum() + 1;
+	let top = Math.ceil(tl / fl);
 	if (n <= top) {
 		$("#list").val(n);
 		chapter();
 	}
 }
 
-function BtnDisable(){ 
-	var n = vocNum();
-	var nt = Math.ceil(tl / fl);
-	
+function BtnDisable() {
+	let n = vocNum();
+	let nt = Math.ceil(tl / fl);
+
 	if (n <= 1) {
-		$("#Previous").attr("disabled",true);
-		$("#Next").attr("disabled",false);
+		$("#Previous").attr("disabled", true);
+		$("#Next").attr("disabled", false);
 	}
-	else if (n >= nt){ 
-		$("#Previous").attr("disabled",false);
-		$("#Next").attr("disabled",true);
+	else if (n >= nt) {
+		$("#Previous").attr("disabled", false);
+		$("#Next").attr("disabled", true);
 	}
 	else {
-		$("#Previous").attr("disabled",false);
-		$("#Next").attr("disabled",false);
+		$("#Previous").attr("disabled", false);
+		$("#Next").attr("disabled", false);
 	}
-	
-}
- 
-function ClearTable(){
-	var table = document.getElementById("ans");
-	var rows = table.rows.length;
-	
-    for(i = 0 ; i < rows - 1 ; i++){
-		var oi = document.getElementById("O" + (i + 1));
-		var tr = oi.parentNode;
-		tr.parentNode.removeChild(tr);
-    }
-}  
- 
-function InitTable(){
-	
-	var k = fl * vocNum(); 
-	var n = fl - ((k > tl) ? (k - tl) : 0); 
 
-	var tab = document.getElementById("ans");
-	var tb = tab.getElementsByTagName("tbody")[0];
+}
+
+function ClearTable() {
+	let table = document.getElementById("ans");
+	let rows = table.rows.length;
+
+	for (i = 0; i < rows - 1; i++) {
+		let oi = document.getElementById("O" + (i + 1));
+		let tr = oi.parentNode;
+		tr.parentNode.removeChild(tr);
+	}
+}
+
+function InitTable() {
+
+	let k = fl * vocNum();
+	let n = fl - ((k > tl) ? (k - tl) : 0);
+
+	let tab = document.getElementById("ans");
+	let tb = tab.getElementsByTagName("tbody")[0];
 	document.getElementById("keys").innerHTML = "Options";
-	
-	for (var i = 0 ; i < n ; i++){
-		
-		var tr = document.createElement("tr");
-		 			
-		var td1 = document.createElement("td");
-		var td2 = document.createElement("td"); 
+
+	for (let i = 0; i < n; i++) {
+
+		let tr = document.createElement("tr");
+
+		let td1 = document.createElement("td");
+		let td2 = document.createElement("td");
 		td2.className = "sheet";
 		td2.id = "O" + (i + 1);
-		var td3 = document.createElement("td");
+		let td3 = document.createElement("td");
 		td3.className = "sheet";
 		td3.id = "K" + (i + 1);
-		
-		var p = document.createElement("p");
-		p.id = "Q" + (i + 1); 		
-		var sp = document.createElement("span");
+
+		let p = document.createElement("p");
+		p.id = "Q" + (i + 1);
+		let sp = document.createElement("span");
 		sp.id = "A" + (i + 1);
-		
+
 		td1.appendChild(p);
 		td3.appendChild(sp);
-		
+
 		tr.appendChild(td1);
 		tr.appendChild(td2);
 		tr.appendChild(td3);
-		
-		tb.appendChild(tr); 
-	}
-} 
 
-function loadList(){ 
-	var n = Math.ceil(tl / fl);
-	var lst = document.getElementById("list");
+		tb.appendChild(tr);
+	}
+}
+
+function loadList() {
+	let n = Math.ceil(tl / fl);
+	let lst = document.getElementById("list");
 	document.getElementById("top").innerHTML = n;
-	for (var i = 0 ; i < n ; i++) {
-		var newOpt = document.createElement("option");
+	for (let i = 0; i < n; i++) {
+		let newOpt = document.createElement("option");
 		newOpt.value = i + 1;
 		newOpt.innerHTML = newOpt.value;
 		lst.appendChild(newOpt);
-	} 
-} 
- 
-function loadQA(){
-	
-	var n = fl * (vocNum() - 1); 
-	var j = (n + fl > tl) ? (tl - n) : fl;
-	var m = mess(1, j); 
-	
-	for (var i = 0 ; i < j ; i++) {
-		var qi = document.getElementById("Q" + (i + 1));
-		var ai = document.getElementById("A" + m[i]);
-		qi.innerHTML = QA[n + i][0];
-		ai.innerHTML = randAns(QA[n + i][2], QA[n + i][1]);
-	} 
-	
+	}
 }
 
-function loadKO(){
-	
-	var kn = 0;
-	var n = fl * (vocNum() - 1);
-	var j = (n + fl > tl) ? (tl - n) : fl;
-	
-	for (var i = 0 ; i < j ; i++) {
-		var ans = QA[n + i][1];
-		var opt = QA[n + i][2];
-		kn += updElement("O" + (i + 1), ans, opt);
-		updChild("K" + (i + 1), ans, opt); 
+function loadQA() {
+
+	let n = fl * (vocNum() - 1);
+	let j = (n + fl > tl) ? (tl - n) : fl;
+	let m = mess(1, j);
+
+	for (let i = 0; i < j; i++) {
+		let qi = document.getElementById("Q" + (i + 1));
+		let ai = document.getElementById("A" + m[i]);
+		qi.innerHTML = QA[n + i][0];
+		ai.innerHTML = randAns(QA[n + i][2], QA[n + i][1]);
 	}
-	
+
+}
+
+function loadKO() {
+
+	let kn = 0;
+	let n = fl * (vocNum() - 1);
+	let j = (n + fl > tl) ? (tl - n) : fl;
+
+	for (let i = 0; i < j; i++) {
+		let ans = QA[n + i][1];
+		let opt = QA[n + i][2];
+		kn += updElement("O" + (i + 1), ans, opt);
+		updChild("K" + (i + 1), ans, opt);
+	}
+
 	VaryBtn(kn, j);
-	
-} 
+
+}
 
 
-function VaryBtn(kn, j){ 
-	var sbm = document.getElementById("Submit");
+function VaryBtn(kn, j) {
+	let sbm = document.getElementById("Submit");
 	sbm.id = "Redo";
 	sbm.value = "[ Redo ]";
-	
+
 	document.getElementById("grade").innerHTML = kn + "/" + j;
 	document.getElementById("keys").innerHTML = "Keys";
 }
 
-function RecBtn(){ 
-	var redo = document.getElementById("Redo");
+function RecBtn() {
+	let redo = document.getElementById("Redo");
 	redo.id = "Submit";
 	redo.value = "[ Submit ]";
-	
+
 	document.getElementById("grade").innerHTML = "Answers";
 	document.getElementById("keys").innerHTML = "Options";
 }
 
-function mode(){
-	var url = window.location.href;
-	var isg = parseInt(url.split("?")[1], 10);
+function mode() {
+	let url = window.location.href;
+	let isg = parseInt(url.split("?")[1], 10);
 	return (isg == 0);
-} 
+}
 
-function getlist()
-{
-	var url = window.location.href;
+function getlist() {
+	let url = window.location.href;
 	return url.split("&")[1];;
 }
 
-$(document).on("click","#Next",function(){
-	pageDown(); 
+$(document).on("click", "#Next", function () {
+	pageDown();
 });
 
-$(document).on("click","#Previous",function(){
-	pageUp(); 
+$(document).on("click", "#Previous", function () {
+	pageUp();
 });
 
-$(document).on("change","#list",function(){
+$(document).on("change", "#list", function () {
 	chapter();
 });
 
-$(document).on("click","#Submit",function(){
+$(document).on("click", "#Submit", function () {
 	loadKO();
 });
 
-$(document).on("click","#Redo",function(){
+$(document).on("click", "#Redo", function () {
 	chapter();
 });
 
-$(document).on("click","#Quit",function(){
+$(document).on("click", "#Quit", function () {
 	window.location.href = "index.html";
 });
 
-window.onload = function() {
-	wordlist(mode(), getlist()); 
+window.onload = function () {
+	wordlist(mode(), getlist());
 	loadList();
 	InitTable();
 	loadQA();
-	InitDrag(); 
+	InitDrag();
 	BtnDisable();
 }
